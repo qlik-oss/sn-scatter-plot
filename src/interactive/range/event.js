871d7bd8cd34fa9e2db8bb2b28c25c11a854b27c
Bao Ho <bao.hodang@gmail.com>
2021-04-23 11:47:42 +0200

    feat: range move performance

--- src/interactive/range/event.js
+++ src/interactive/range/event.js
@@ -31,6 +31,7 @@ const range = ({ eventName, key, fillTargets = [], requireFailure, recognizeWith
         this.started = eventName;
         return true;
       }
+      // console.log(`${e.center.x}, ${e.center.y}`);
 
       return components.length > 0;
     },
@@ -49,6 +50,7 @@ const range = ({ eventName, key, fillTargets = [], requireFailure, recognizeWith
     [`${eventName}move`](e) {
       e.preventDefault();
       this.chart.component(key).emit('rangeMove', e);
+      // console.log('1: key');
     },
     [`${eventName}end`](e) {
       if (this.chart.brushSelectionIncludeMax === undefined) {
