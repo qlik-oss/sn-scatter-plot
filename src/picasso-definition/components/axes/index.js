728ce2270a2d9bad84e81a5403d467cffb00a3ca
costa <costantino.marullo@qlik.com>
2021-05-03 11:14:32 +0200

    fix: show axis when labels&titles is none

--- src/picasso-definition/components/axes/index.js
+++ src/picasso-definition/components/axes/index.js
@@ -6,66 +6,64 @@ export default function createAxes({ layoutModel, dockModel, themeModel }) {
 
   const style = themeModel.query.getStyle();
 
-  const xAxisDefinition =
-    !xAxis || xAxis.show === 'none'
-      ? false
-      : {
-          type: 'axis',
-          key: KEYS.COMPONENT.X_AXIS,
-          scale: KEYS.SCALE.X,
-          layout: {
-            dock: dockModel.x.dock,
-            minimumLayoutMode: MODES.AXIS.X,
+  const xAxisDefinition = !xAxis
+    ? false
+    : {
+        type: 'axis',
+        key: KEYS.COMPONENT.X_AXIS,
+        scale: KEYS.SCALE.X,
+        layout: {
+          dock: dockModel.x.dock,
+          minimumLayoutMode: MODES.AXIS.X,
+        },
+        settings: {
+          labels: {
+            show: xAxis.show !== 'title' && xAxis.show !== 'none',
+            fontFamily: style.axis.label.name.fontFamily,
+            fontSize: style.axis.label.name.fontSize,
+            fill: style.axis.label.name.color,
           },
-          settings: {
-            labels: {
-              show: xAxis.show !== 'title',
-              fontFamily: style.axis.label.name.fontFamily,
-              fontSize: style.axis.label.name.fontSize,
-              fill: style.axis.label.name.color,
-            },
-            line: {
-              stroke: style.axis.line.major.color,
-            },
-            ticks: {
-              stroke: style.axis.line.major.color,
-            },
-            minorTicks: {
-              stroke: style.axis.line.minor.color,
-            },
-            paddingEnd: 4,
+          line: {
+            stroke: style.axis.line.major.color,
           },
-        };
+          ticks: {
+            stroke: style.axis.line.major.color,
+          },
+          minorTicks: {
+            stroke: style.axis.line.minor.color,
+          },
+          paddingEnd: 4,
+        },
+      };
 
-  const yAxisDefinition =
-    !yAxis || yAxis.show === 'none'
-      ? false
-      : {
-          type: 'axis',
-          key: KEYS.COMPONENT.Y_AXIS,
-          scale: KEYS.SCALE.Y,
-          layout: {
-            dock: dockModel.y.dock,
-            minimumLayoutMode: MODES.AXIS.Y,
+  const yAxisDefinition = !yAxis
+    ? false
+    : {
+        type: 'axis',
+        key: KEYS.COMPONENT.Y_AXIS,
+        scale: KEYS.SCALE.Y,
+        layout: {
+          dock: dockModel.y.dock,
+          minimumLayoutMode: MODES.AXIS.Y,
+        },
+        settings: {
+          labels: {
+            show: yAxis.show !== 'title' && yAxis.show !== 'none',
+            fontFamily: style.axis.label.name.fontFamily,
+            fontSize: style.axis.label.name.fontSize,
+            fill: style.axis.label.name.color,
           },
-          settings: {
-            labels: {
-              show: yAxis.show !== 'title',
-              fontFamily: style.axis.label.name.fontFamily,
-              fontSize: style.axis.label.name.fontSize,
-              fill: style.axis.label.name.color,
-            },
-            line: {
-              stroke: style.axis.line.major.color,
-            },
-            ticks: {
-              stroke: style.axis.line.major.color,
-            },
-            minorTicks: {
-              stroke: style.axis.line.minor.color,
-            },
+          line: {
+            stroke: style.axis.line.major.color,
           },
-        };
+          ticks: {
+            stroke: style.axis.line.major.color,
+          },
+          minorTicks: {
+            stroke: style.axis.line.minor.color,
+          },
+        },
+      };
 
   return [xAxisDefinition, yAxisDefinition];
 }
