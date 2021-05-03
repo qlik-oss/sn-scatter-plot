fe754f538a54c88bd3ff6b474df16a8a3838143c
quanho <quan.ho@qlik.com>
2021-05-03 10:27:03 +0200

    refactor: update unit tests

--- src/models/theme-model/style/table/__tests__/index.spec.js
+++ src/models/theme-model/style/table/__tests__/index.spec.js
@@ -3,7 +3,7 @@ import createTable from '..';
 describe('theme-model table', () => {
   it('should create a table', () => {
     const table = createTable();
-    const tableObjectsTotal = 13;
+    const tableObjectsTotal = 22;
     expect(table.length).to.equal(tableObjectsTotal);
   });
 });
