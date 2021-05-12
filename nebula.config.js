b5b4ad7fda3351423f724244cfb515de34f8b691
Quan Ho <quan.ho@qlik.com>
2021-05-12 16:05:33 +0200

    refactor: refactor rendering tests (#99)

--- nebula.config.js
+++ nebula.config.js
@@ -1,21 +1,18 @@
 const path = require('path');
+const fs = require('fs-extra');
 
 const sourcePath = [__dirname, 'test', 'rendering', 'data'];
 const source = path.resolve(...sourcePath);
-const mes_2_color_dim = require(path.resolve(source, 'mes_2_color_dim.json')); // eslint-disable-line
+const renderConfigs = fs.readdirSync('test/rendering/data').map((file) => ({
+  id: file.replace('.json', ''),
+  render: {
+    options: { renderer: 'svg' },
+    properties: require(path.resolve(source, file)), // eslint-disable-line
+  },
+}));
 
 module.exports = {
   serve: {
-    renderConfigs: [
-      {
-        id: 'mes_2_color_dim',
-        render: {
-          options: {
-            renderer: 'svg',
-          },
-          properties: mes_2_color_dim,
-        },
-      },
-    ],
+    renderConfigs,
   },
 };
--- test/rendering/index.spec.js
+++ test/rendering/index.spec.js
@@ -1,3 +1,5 @@
+const fs = require('fs-extra');
+
 const OPTS = {
   artifactsPath: 'test/rendering/__artifacts__',
 };
@@ -6,21 +8,19 @@ async function takeScreenshot(elm) {
   return page.screenshot({ clip: await elm.boundingBox() });
 }
 
-describe('interaction', () => {
+describe('rendering', () => {
   const content = '.njs-viz[data-render-count="1"]';
   const app = encodeURIComponent(process.env.APP_ID || '/apps/Executive_Dashboard.qvf');
-  let elm;
-
-  beforeEach(async () => {
-    await page.goto(`${process.env.BASE_URL}/render/?app=${app}&render-config=mes_2_color_dim`);
-    elm = await page.waitForSelector(content, {
-      timeout: 5000,
+  fs.readdirSync('test/rendering/data').forEach((file) => {
+    const name = file.replace('.json', '');
+    it(name, async function run() {
+      await page.goto(`${process.env.BASE_URL}/render/?app=${app}&render-config=${name}`);
+      const elm = await page.waitForSelector(content, {
+        timeout: 5000,
+      });
+      this.timeout(10000);
+      const img = await takeScreenshot(elm);
+      return expect(img).to.matchImageOf(name, OPTS, 0.0005);
     });
-  });
-
-  it('Measures: 2, color: by dimension', async function run() {
-    this.timeout(10000);
-    const img = await takeScreenshot(elm);
-    return expect(img).to.matchImageOf('mes_2_color_dim', OPTS, 0.0005);
   });
 });
