a94b2aba12e22cca03e49cacfdf2f46becc6d0ac
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2021-05-17 11:50:55 +0200

    chore: fix copy target on windows (#96)

--- copy-ext.js
+++ copy-ext.js
@@ -5,6 +5,9 @@ const fs = require('fs-extra');
 const sourcePath = [__dirname, 'dist'];
 const source = path.resolve(...sourcePath);
 const targetPath = [os.homedir(), 'Qlik', 'Sense', 'Extensions', 'sn-scatter-plot'];
+if (os.platform() === 'win32') {
+  targetPath.splice(1, 0, 'Documents');
+}
 const target = path.resolve(...targetPath);
 const files = ['sn-scatter-plot.js', 'sn-scatter-plot.js.map', 'sn-scatter-plot.qext'];
 
