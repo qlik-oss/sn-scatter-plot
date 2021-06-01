ebc854c7e9e224192cbd32ea26d051a6e06eccfc
Bao Ho <70384379+bao-ho@users.noreply.github.com>
2021-06-01 11:59:25 +0200

    refactor: unit tests (#163)

--- src/models/chart-model/__tests__/index.spec.js
+++ src/models/chart-model/__tests__/index.spec.js
@@ -1,4 +1,5 @@
 import createChartModel from '..';
+import * as createViewHandler from '../../../view-handler';
 
 describe('chart-model', () => {
   let sandbox;
@@ -16,12 +17,12 @@ describe('chart-model', () => {
   let viewHandler;
   let viewState;
   let extremumModel;
-  let getViewHandlerFn;
 
   beforeEach(() => {
     sandbox = sinon.createSandbox();
     global.requestAnimationFrame = (cb) => setTimeout(cb, 20);
     viewHandler = { getMeta: sandbox.stub().returns('isHomeState') };
+    sandbox.stub(createViewHandler, 'default').returns(viewHandler);
     viewState = {
       get() {
         return this.props;
@@ -35,6 +36,7 @@ describe('chart-model', () => {
     };
     chart = {
       update: sandbox.stub(),
+      layoutComponents: sandbox.stub(),
     };
     localeInfo = { key: 'locale-info' };
     hyperCube = {
@@ -72,7 +74,6 @@ describe('chart-model', () => {
         viewState,
         extremumModel,
       });
-    getViewHandlerFn = sandbox.stub(create().query, 'getViewHandler').returns(viewHandler);
   });
 
   afterEach(() => {
@@ -121,7 +122,7 @@ describe('chart-model', () => {
 
     describe('getViewHandler', () => {
       it('should return correct view handler object', () => {
-        expect(getViewHandlerFn()).to.have.all.keys(['getMeta']);
+        expect(create().query.getViewHandler().getMeta()).to.equal('isHomeState');
       });
     });
 
@@ -134,6 +135,12 @@ describe('chart-model', () => {
     describe('isInteractionInProgess', () => {
       it('should return correct isInteractionInProgess value', () => {
         expect(create().query.isInteractionInProgess()).to.deep.equal(false);
+      });
+    });
+
+    describe('isPrelayout', () => {
+      it('should return correct isPrelayout value', () => {
+        expect(create().query.isPrelayout()).to.equal(true);
       });
     });
 
@@ -150,9 +157,26 @@ describe('chart-model', () => {
     });
   });
 
-  describe.skip('command', () => {
+  describe('command', () => {
     it('should expose correct methods', () => {
-      expect(create().command).to.have.all.keys(['update']);
+      expect(create().command).to.have.all.keys(['layoutComponents', 'update']);
+    });
+
+    describe('layoutComponents', () => {
+      it('should call layoutComponents correctly', () => {
+        create().command.layoutComponents({ settings: { key: 'settings' } });
+        const argsObject = chart.layoutComponents.args[0][0];
+
+        expect(chart.layoutComponents).to.have.been.calledOnce;
+        expect(argsObject.data).to.be.an('array');
+        expect(argsObject.data[0].config.localeInfo).to.equal(localeInfo);
+        expect(argsObject.settings).eql({ key: 'settings' });
+      });
+
+      it('should call layoutComponents, when settings is implicit', () => {
+        create().command.layoutComponents();
+        expect(chart.layoutComponents).to.have.been.calledOnce;
+      });
     });
 
     describe('update', () => {
