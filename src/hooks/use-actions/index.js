b2a209cb0e38df6ba23a3433097fef2f2aa48f8b
Quan Ho <quan.ho@qlik.com>
2021-06-14 09:00:42 +0200

    fix: remove timming issue (#198)

--- src/hooks/use-actions/index.js
+++ src/hooks/use-actions/index.js
@@ -1,39 +1,25 @@
-import { useConstraints, useState, useEffect } from '@nebula.js/stardust';
+import { useConstraints, useState } from '@nebula.js/stardust';
 import useLasso from './use-lasso';
 import useSelect from './use-select';
 
 export default function useActions({ flags }) {
   const constraints = useConstraints();
-  const [zoom, setZoom] = useState(false);
-  const [scroll, setScroll] = useState(false);
-  const [interact, setInteract] = useState(false);
-  const [tooltip, setTooltip] = useState(false);
   const select = useSelect();
   const lasso = useLasso();
   const enabledPanZoom = flags.isEnabled('panZoom');
 
-  useEffect(() => {
-    if (!constraints) {
-      return;
-    }
-    setZoom(enabledPanZoom && !constraints.active);
-    setScroll(!constraints.active);
-    setInteract(!constraints.active);
-    setTooltip(!constraints.passive);
-  }, [constraints]);
-
   const [actions] = useState({});
   actions.zoom = {
-    enabled: () => zoom,
+    enabled: () => enabledPanZoom && constraints && !constraints.active,
   };
   actions.scroll = {
-    enabled: () => scroll,
+    enabled: () => constraints && !constraints.active,
   };
   actions.interact = {
-    enabled: () => interact,
+    enabled: () => constraints && !constraints.active,
   };
   actions.tooltip = {
-    enabled: () => tooltip,
+    enabled: () => constraints && !constraints.passive,
   };
   actions.lasso = lasso;
   actions.select = select;
