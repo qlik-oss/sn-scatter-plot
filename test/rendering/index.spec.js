c22b8011caafd4764f016e85962967536da2a877
Bao Ho <bao.hodang@gmail.com>
2021-08-26 16:58:51 +0200

    fix: adjust timeout position

--- test/rendering/index.spec.js
+++ test/rendering/index.spec.js
@@ -14,10 +14,10 @@ describe('rendering', () => {
   fs.readdirSync('test/rendering/data').forEach((file) => {
     const name = file.replace('.json', '');
     it(name, async function run() {
+      this.timeout(20000);
       await page.goto(`${process.env.BASE_URL}/render/?app=${app}&render-config=${name}`);
       await page.waitForSelector(content, { visible: true, timeout: 5000 });
       const elm = await page.$(content);
-      this.timeout(20000);
       const img = await takeScreenshot(elm);
       return expect(img).to.matchImageOf(name, OPTS, 0.0005);
     });
