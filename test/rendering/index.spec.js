79599c8c8742b6cd23d37f26989e2ebff5c803e4
Bao Ho <bao.hodang@gmail.com>
2021-08-28 00:56:42 +0200

    refactor: plugin rendering tests

--- test/rendering/index.spec.js
+++ test/rendering/index.spec.js
@@ -1,6 +1,5 @@
 const fs = require('fs-extra');
-
-// const { resolve } = require('path');
+const { resolve } = require('path');
 
 const OPTS = {
   artifactsPath: 'test/rendering/__artifacts__',
@@ -48,7 +47,6 @@ describe('rendering', () => {
   fs.readdirSync('test/rendering/data').forEach((file) => {
     const name = file.replace('.json', '');
     it(name, async () => {
-      console.log(await myPage.browser().version());
       await myPage.goto(`${process.env.BASE_URL}/render/?app=${app}&render-config=${name}`);
       // this.timeout(10000);
       const elm = await myPage.waitForSelector(content, { visible: true, timeout: 5000 });
@@ -56,52 +54,40 @@ describe('rendering', () => {
       return expect(img).to.matchImageOf(name, OPTS, 0.0005);
     });
   });
+
+  it('should render lines plugin correctly', async function run() {
+    const absolutePath = resolve(__dirname, '../../examples/plugins/lines/index.html');
+    const localURL = `file://${absolutePath}`;
+    await myPage.goto(localURL);
+    const elm = await myPage.waitForSelector(content, {
+      timeout: 5000,
+    });
+    this.timeout(5000);
+    const img = await takeScreenshot(elm);
+    return expect(img).to.matchImageOf('lines_plugin', OPTS, 0.0005);
+  });
+
+  it('should render config1 correctly', async function run() {
+    const absolutePath = resolve(__dirname, '../../examples/plugins/configurable/config1.html');
+    const localURL = `file://${absolutePath}`;
+    await myPage.goto(localURL);
+    const elm = await myPage.waitForSelector(content, {
+      timeout: 5000,
+    });
+    this.timeout(5000);
+    const img = await takeScreenshot(elm);
+    return expect(img).to.matchImageOf('config1', OPTS, 0.0005);
+  });
+
+  it('should render config2 correctly', async function run() {
+    const absolutePath = resolve(__dirname, '../../examples/plugins/configurable/config2.html');
+    const localURL = `file://${absolutePath}`;
+    await myPage.goto(localURL);
+    const elm = await myPage.waitForSelector(content, {
+      timeout: 5000,
+    });
+    this.timeout(5000);
+    const img = await takeScreenshot(elm);
+    return expect(img).to.matchImageOf('config2', OPTS, 0.0005);
+  });
 });
-
-// // const OPTS = {
-// //   artifactsPath: 'test/rendering-plugin/lines/__artifacts__',
-// // };
-
-// describe('rendering lines plugin example', () => {
-//   it('should render lines plugin correctly', async function run() {
-//     const absolutePath = resolve(__dirname, '../../../examples/plugins/lines/index.html');
-//     const localURL = `file://${absolutePath}`;
-//     await page.goto(localURL);
-//     const elm = await page.waitForSelector(content, {
-//       timeout: 5000,
-//     });
-//     this.timeout(5000);
-//     const img = await takeScreenshot(elm);
-//     return expect(img).to.matchImageOf('lines_plugin', OPTS, 0.0005);
-//   });
-// });
-
-// // const OPTS = {
-// //   artifactsPath: 'test/rendering-plugin/configurable/__artifacts__',
-// // };
-
-// describe('rendering configurable plugin examples', () => {
-//   it('should render config1 correctly', async function run() {
-//     const absolutePath = resolve(__dirname, '../../../examples/plugins/configurable/config1.html');
-//     const localURL = `file://${absolutePath}`;
-//     await page.goto(localURL);
-//     const elm = await page.waitForSelector(content, {
-//       timeout: 5000,
-//     });
-//     this.timeout(5000);
-//     const img = await takeScreenshot(elm);
-//     return expect(img).to.matchImageOf('config1', OPTS, 0.0005);
-//   });
-
-//   it('should render config2 correctly', async function run() {
-//     const absolutePath = resolve(__dirname, '../../../examples/plugins/configurable/config2.html');
-//     const localURL = `file://${absolutePath}`;
-//     await page.goto(localURL);
-//     const elm = await page.waitForSelector(content, {
-//       timeout: 5000,
-//     });
-//     this.timeout(5000);
-//     const img = await takeScreenshot(elm);
-//     return expect(img).to.matchImageOf('config2', OPTS, 0.0005);
-//   });
-// });
