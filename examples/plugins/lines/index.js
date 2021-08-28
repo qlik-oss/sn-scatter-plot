415eb1fd9c90728d66340204ee95387137e13a83
Bao Ho <bao.hodang@gmail.com>
2021-08-28 12:24:50 +0200

    fix: update enigma and enigma schemes

--- examples/plugins/lines/index.js
+++ examples/plugins/lines/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 (function run() {
   function connect() {
-    const schemaPromise = fetch('https://unpkg.com/enigma.js/schemas/12.170.2.json').then((response) =>
+    const schemaPromise = fetch('https://unpkg.com/enigma.js/schemas/12.34.11.json').then((response) =>
       response.json()
     );
 
