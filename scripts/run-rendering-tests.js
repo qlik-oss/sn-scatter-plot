7f977d6a51adf976f8911096310d4c30f15085c3
Bao Ho <bao.hodang@gmail.com>
2021-09-01 00:30:09 +0200

    fix: new cmd for ci

--- scripts/run-rendering-tests.js
+++ scripts/run-rendering-tests.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 
 const cmd = process.env.CI
-  ? 'aw puppet --glob "test/**/*.spec.js"  --mocha.bail false --mocha.timeout 10000'
+  ? 'aw puppet --glob "test/**/*.spec.js"  --mocha.bail false --mocha.timeout 10000  --chrome.browserWSEndpoint "ws://localhost:3000" --no-launch'
   : 'docker-compose up --build --remove-orphans --exit-code-from rendering-test-runner';
 console.log('Env dependent cmd: ');
 console.log(cmd);
