c2d0b745d8cec0b20da923e62a6dd4cd6015f809
quanho <quan.ho@qlik.com>
2021-11-17 16:09:05 +0100

    feat: add bin selection

--- src/services/selection-service/index.js
+++ src/services/selection-service/index.js
@@ -1,9 +1,9 @@
+import picassoQ from 'picasso-plugin-q';
 import { selectionService as createSelectionService } from 'qlik-chart-modules';
+import qBrush from './bin-selection/q-brush';
 
 export default function createService({ chart, actions, selections }) {
   const allowSimultaneous = [...Array(15)].map((d, i) => `qHyperCube/qMeasureInfo/${i}`);
-  allowSimultaneous.push('binData/binX');
-  allowSimultaneous.push('binData/binY');
 
   const service = createSelectionService({
     chart,
@@ -13,15 +13,19 @@ export default function createService({ chart, actions, selections }) {
       allowSimultaneous,
       selectionActions: {
         clear: ({ clearMinor, clearLegend, selectionInfo, cleared }) => {
-          const isSelectingRanges = ['xRange', 'yRange', 'binXRange', 'binYRange'].includes(selectionInfo.event);
+          const isSelectingRanges = ['xRange', 'yRange'].includes(selectionInfo.event);
           if (cleared || !isSelectingRanges) {
             clearMinor({ eventName: 'xRange', componentName: 'x-range-brush' });
             clearMinor({ eventName: 'yRange', componentName: 'y-range-brush' });
-            clearMinor({ eventName: 'binXRange', componentName: 'bin-x-range-brush' });
-            clearMinor({ eventName: 'binYRange', componentName: 'bin-y-range-brush' });
           }
           clearLegend();
         },
+      },
+      selectionsFn: (brush, qBrushOptions, layout) => {
+        if (layout.qHyperCube.qDataPages.length) {
+          return picassoQ.selections(brush, qBrushOptions, layout);
+        }
+        return qBrush(brush, qBrushOptions, layout);
       },
     },
   });
