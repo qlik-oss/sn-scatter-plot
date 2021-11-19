e5f4b3fbd68c72bb28920aaebdf70aede37f8b90
Jingjing Xie <jingjing.xie@qlik.com>
2021-11-19 14:29:27 +0100

    fix: white gaps between bins (#83)

--- src/picasso-definition/components/heat-map/index.js
+++ src/picasso-definition/components/heat-map/index.js
@@ -32,6 +32,7 @@ export default function createHeatMap({ models, flags }) {
         scale: KEYS.SCALE.HEAT_MAP_COLOR,
         fn: (d) => d.scale(d.datum.binDensity.value),
       },
+      strokeWidth: 0,
       shape: () => ({
         type: 'rect',
         width: binWidthPx,
@@ -48,8 +49,8 @@ export default function createHeatMap({ models, flags }) {
         const binWidth = Math.abs(firstBin.qText[0] - firstBin.qText[2]);
         const binHeight = Math.abs(firstBin.qText[1] - firstBin.qText[3]);
 
-        binWidthPx = (binWidth * size.width) / (dataView.xAxisMax - dataView.xAxisMin);
-        binHeightPx = (binHeight * size.height) / (dataView.yAxisMax - dataView.yAxisMin);
+        binWidthPx = (binWidth * size.width) / (dataView.xAxisMax - dataView.xAxisMin) + 0.5;
+        binHeightPx = (binHeight * size.height) / (dataView.yAxisMax - dataView.yAxisMin) + 0.5;
       }
     },
   };
