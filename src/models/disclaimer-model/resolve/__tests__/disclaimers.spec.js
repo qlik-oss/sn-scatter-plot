3292e45a58378c716bd847fefcb711928c397870
Bao Ho <bao.hodang@gmail.com>
2021-11-24 20:27:06 +0100

    fix: unit tests disclaimer model

--- src/models/disclaimer-model/resolve/__tests__/disclaimers.spec.js
+++ src/models/disclaimer-model/resolve/__tests__/disclaimers.spec.js
@@ -1,4 +1,40 @@
 // eslint-disable-next-line no-unused-vars
 import disclaimers from '../disclaimers';
 
-describe('disclaimers', () => {});
+describe('disclaimers', () => {
+  let sandbox;
+  let layoutService;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    layoutService = { getHyperCubeValue: sandbox.stub() };
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should contain four elemens', () => {
+    expect(disclaimers.length).to.equal(4);
+  });
+
+  describe('NoDataExist', () => {
+    it('should have all keys', () => {
+      expect(disclaimers[0]).to.have.all.keys(['key', 'alignment', 'condition']);
+    });
+
+    describe('condition', () => {
+      it('should return true if qcx or qcy is zero', () => {
+        layoutService.getHyperCubeValue.returns({ qcx: 0, qcy: 1000 });
+        expect(disclaimers[0].condition({ layoutService })).to.equal(true);
+        layoutService.getHyperCubeValue.returns({ qcx: 2000, qcy: 0 });
+        expect(disclaimers[0].condition({ layoutService })).to.equal(true);
+      });
+
+      it('should return false if qcx and qcy are not zero', () => {
+        layoutService.getHyperCubeValue.returns({ qcx: 1, qcy: 1000 });
+        expect(disclaimers[0].condition({ layoutService })).to.equal(false);
+      });
+    });
+  });
+});
