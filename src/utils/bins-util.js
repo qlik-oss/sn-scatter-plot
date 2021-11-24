0a82b4a6fedfe213a2e40562a52b31e095183cb9
Jingjing Xie <jingjing.xie@qlik.com>
2021-11-24 14:15:41 +0100

    fix: remove comment and console log

--- src/utils/bins-util.js
+++ src/utils/bins-util.js
@@ -4,23 +4,20 @@ const populateBins = (dataPages, bins) => {
   const matrix = dataPages[0].qMatrix;
   let i;
 
-  // Hack for snapshot
   if (dataPages[0].reformatted) {
     for (i = 1; i < matrix.length; i++) {
       try {
         bins.push(matrix[i][0]);
-      } catch (err) {
-        // console.log( err );
-      }
+        // eslint-disable-next-line no-empty
+      } catch (err) {}
     }
   } else {
     for (i = 1; i < matrix.length; i++) {
       try {
         matrix[i][0].qText = JSON.parse(matrix[i][0].qText);
         bins.push(matrix[i][0]);
-      } catch (err) {
-        // console.log( err );
-      }
+        // eslint-disable-next-line no-empty
+      } catch (err) {}
     }
     // eslint-disable-next-line no-param-reassign
     dataPages[0].reformatted = true;
