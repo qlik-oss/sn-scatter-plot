c1327ea7a6fcda25482f1b276758126020c5e183
Bao Ho <bao.hodang@gmail.com>
2021-12-14 10:41:00 +0100

    fix: add new tests

--- src/picasso-definition/components/disclaimer/__tests__/index.spec.js
+++ src/picasso-definition/components/disclaimer/__tests__/index.spec.js
@@ -1,4 +1,66 @@
 // eslint-disable-next-line no-unused-vars
 import createDisclaimer from '..';
 
-describe('disclaimer', () => {});
+describe('createDisclaimer', () => {
+  let sandbox;
+  let create;
+  let disclaimerModel;
+  let layoutService;
+  let context;
+  let picasso;
+  let formatter;
+  let formatterFactory;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    disclaimerModel = {
+      query: { getDisclaimer: sandbox.stub().returns({ key: 'OverviewData', alignment: 'bottom' }) },
+    };
+    layoutService = { getHyperCubeValue: sandbox.stub() };
+    context = { rtl: 'ltr', translator: { get: sandbox.stub() } };
+    context.translator.get
+      .withArgs('properties.compression.providingOverviewOf', 'overview-data-disclaimer-text')
+      .returns('translated-overview-data-disclaimer-text');
+    context.translator.get.withArgs('Object.Disclaimer.Others').returns('translated-other-disclaimer-text');
+    formatter = sandbox.stub().returns('overview-data-disclaimer-text');
+    formatterFactory = sandbox.stub().returns(formatter);
+    picasso = { formatter: sandbox.stub().returns(formatterFactory) };
+    create = () => createDisclaimer({ disclaimerModel, context, layoutService, picasso });
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should return correct disclaimer component, when it is OverviewData', () => {
+    expect(create()).to.deep.equal({
+      key: 'disclaimer',
+      type: 'disclaimer',
+      dock: 'bottom',
+      show: true,
+      settings: {
+        label: 'translated-overview-data-disclaimer-text',
+        rtl: 'ltr',
+      },
+    });
+  });
+
+  it('should return correct disclaimer component, when it is other than OverviewData', () => {
+    disclaimerModel.query.getDisclaimer.returns({ key: 'Others', alignment: 'top' });
+    expect(create()).to.deep.equal({
+      key: 'disclaimer',
+      type: 'disclaimer',
+      dock: 'top',
+      show: true,
+      settings: {
+        label: 'translated-other-disclaimer-text',
+        rtl: 'ltr',
+      },
+    });
+  });
+
+  it('should return false, when there is no disclaimer', () => {
+    disclaimerModel.query.getDisclaimer.returns(null);
+    expect(create()).to.equal(false);
+  });
+});
--- src/services/color-service/__tests__/index.spec.js
+++ src/services/color-service/__tests__/index.spec.js
@@ -1,4 +1,54 @@
 // eslint-disable-next-line no-unused-vars
+import * as chartModules from 'qlik-chart-modules';
 import createColorService from '..';
+import * as createLegend from '../legend';
 
-describe('color-service', () => {});
+describe('color-service', () => {
+  let sandbox;
+  let create;
+  let actions;
+  let localeInfo;
+  let app;
+  let chart;
+  let theme;
+  let translator;
+  let options;
+  let model;
+  let layoutService;
+  let picasso;
+  let viewState;
+  let dataHandler;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    sandbox.stub(chartModules, 'colorService').returns('color-service');
+    layoutService = {
+      getHyperCube: sandbox.stub().returns({ qDimensionInfo: [{ baseColor: 'red' }] }),
+      getLayoutValue: sandbox.stub(),
+    };
+    sandbox.stub(createLegend, 'default').returns({ components: '', interactions: '', scales: '' });
+    create = () =>
+      createColorService({
+        actions,
+        localeInfo,
+        app,
+        chart,
+        theme,
+        translator,
+        options,
+        model,
+        layoutService,
+        picasso,
+        viewState,
+        dataHandler,
+      });
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should return correct colorService', () => {
+    expect(create()).to.equal('color-service');
+  });
+});
