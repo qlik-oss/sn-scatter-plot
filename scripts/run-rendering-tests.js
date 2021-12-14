1ba2b5b95f366a7ba7d72bcfae6ae4dffde5822d
Bao Ho <bao.hodang@gmail.com>
2021-12-14 10:45:11 +0100

    fix: delete old script

--- scripts/run-rendering-tests.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-
-const cmd = process.env.CI
-  ? 'aw puppet --glob "test/**/*.spec.js"  --mocha.bail false --mocha.timeout 10000  --chrome.browserWSEndpoint "ws://localhost:3000" --no-launch'
-  : 'docker-compose up --build --remove-orphans --abort-on-container-exit';
-console.log('Env dependent cmd: ');
-console.log(cmd);
-execSync(cmd, { stdio: 'inherit' });
