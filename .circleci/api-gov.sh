857adef64a2e6785e94f2ad75240533266bc7374
Raul Villora Valencia <raulwalencia@gmail.com>
2022-01-24 10:32:13 +0100

    chore: api compliance update

--- .circleci/api-gov.sh
+++ .circleci/api-gov.sh
@@ -1,7 +1,15 @@
 docker cp $1 specs:/specs
+
+apiId=$3; specPath=$2; versionPath=$4
+
+# Get the latest version
+VER=v$(cat ${versionPath} | jq -r '.version')
+
 docker run --volumes-from specs \
--e target=Colony \
--e runMode=failBuild \
--e apiId=$3 \
--e specPath=$2 \
--e commit="$CIRCLE_SHA1" ghcr.io/qlik-trial/asmquery:latest
+-e SPEC_PATHS="$3@/$2" \
+-e COMMIT_SHA="$CIRCLE_SHA1" \
+-e RELEASE_TAG="$VER" \
+-e CREDENTIALS_S3_SECRETKEY="$APICULTURIST_S3" \
+-e CREDENTIALS_GITHUB="$APICULTURIST_GITHUB" \
+-e CREDENTIALS_COLONY="$APICULTURIST_TOKEN" \
+ghcr.io/qlik-download/api-compliance
