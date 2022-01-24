65f04e36c159af254eee427a80d71247e07e07db
Joakim <84327473+joakim-ronning@users.noreply.github.com>
2022-01-24 14:38:47 +0100

    chore: update api compliance (#195)
    * chore: update api compliance
    
    Hello!
    We are migrating to a new image for doing API compliance.  
    Check that the paths and names are correct to your repo.If you have any questions don't hesitate to reach out.
    
    * chore: removed unused file

--- .circleci/api-gov.sh
+++
@@ -1,7 +0,0 @@
-docker cp $1 specs:/specs
-docker run --volumes-from specs \
--e target=Colony \
--e runMode=failBuild \
--e apiId=$3 \
--e specPath=$2 \
--e commit="$CIRCLE_SHA1" ghcr.io/qlik-trial/asmquery:latest
