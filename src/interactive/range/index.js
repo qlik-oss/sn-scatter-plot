6580b8926b891d7095d50de5b231bd9cbb71f2a2
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2022-01-28 13:19:02 +0100

    feat: edit range select bubbles (#213)

--- src/interactive/range/index.js
+++ src/interactive/range/index.js
@@ -1,12 +1,14 @@
 import component from './visual';
 import event from './event';
+import tap from './range-tap';
 
 export default function createRange(config, opts) {
   const c = component(config);
   const e = event(config, opts);
+  const t = config.onEdited ? tap(config, opts) : null;
 
   return {
-    interactions: [e].filter(Boolean),
+    interactions: [e, t].filter(Boolean),
     components: [c],
   };
 }
