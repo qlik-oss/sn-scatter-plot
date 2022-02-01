14ecca68581d108d5cc9196c7478543c73522e7f
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2022-02-01 14:43:12 +0100

    fix: clean up trendlines on conversion (#211)

--- src/ext/property-definition/trendlines-definition.js
+++ src/ext/property-definition/trendlines-definition.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 import { trendlinesService } from 'qlik-chart-modules';
 
-const onGlobalChange = (properties) => {
+export function updateTrendlines(properties) {
   if (properties.qHyperCubeDef?.qMeasures?.length >= 2) {
     properties.qHyperCubeDef.qMeasures[0].qTrendLines = [];
     properties.qHyperCubeDef.qMeasures[1].qTrendLines = [];
@@ -15,7 +15,13 @@ const onGlobalChange = (properties) => {
       }
     });
   }
-};
+}
+
+export function clearTrendlines(properties) {
+  properties.qHyperCubeDef?.qMeasures?.forEach((measureDef) => {
+    measureDef.qTrendLines = [];
+  });
+}
 
 export default function getDefinition(env) {
   const { flags, translator } = env;
@@ -26,7 +32,7 @@ export default function getDefinition(env) {
 
   trendlines.ref = 'trendLines';
   trendlines.schemaIgnore = true;
-  trendlines.globalChange = onGlobalChange;
+  trendlines.globalChange = updateTrendlines;
   trendlines.show = () => flags.isEnabled('BEST_FIT_LINE');
   trendlines.items.type.defaultValue = 'LINEAR';
   trendlines.items.flipXY = {
