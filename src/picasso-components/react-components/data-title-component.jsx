3def28783a4c84d30df526640c28dd0a422e8334
Rajesh <97510507+iqi-qlik@users.noreply.github.com>
2022-07-01 10:46:17 +0530

    Update data-title-component.jsx
    added generic insrtuction for other charts if popover value is null

--- src/picasso-components/react-components/data-title-component.jsx
+++ src/picasso-components/react-components/data-title-component.jsx
@@ -243,7 +243,7 @@ export default function createDataTitileComp() {
       }
 
       const dir = rtlUtils.detectTextDirection(titleData.text);
-      let instruction = '';
+      let instruction = `Currently ${titleData.text} is selected. Click to expand or press enter key to open the list, selecting an option from this list will change the data in the chart`;
       if (popover != null) {
         if (popover.dock === 'right') {
           instruction = `the y-axis is showing ${titleData.text}, Click to expand or press enter key to open the list, selecting an option from this list will change the data in the y-axis shown in the chart`;
