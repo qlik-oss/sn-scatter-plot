86b321eac0d0b1a159b7a7589dac4c1241dda07f
Quan Ho <quan.ho@qlik.com>
2022-11-14 16:40:17 +0100

    fix: add sanity check for no selection (#356)

--- src/utils/remove-listeners.js
+++ src/utils/remove-listeners.js
@@ -1,4 +1,7 @@
 const removeListeners = (emitter, listeners) => {
+  if (!emitter) {
+    return;
+  }
   Object.keys(listeners).forEach((type) => {
     emitter.removeListener(type, listeners[type]);
   });
