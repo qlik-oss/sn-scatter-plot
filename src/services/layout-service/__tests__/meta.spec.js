a00cb01fb83bedf955f214a974a88cb22fdbc216
Christian Veinfors <cvs@qlik.com>
2022-12-16 09:20:50 +0100

    fix(QB-17405): pick up and apply DQ limitations (#365)

--- src/services/layout-service/__tests__/meta.spec.js
+++ src/services/layout-service/__tests__/meta.spec.js
@@ -8,13 +8,11 @@ describe('meta', () => {
   let create;
   let flags;
   let layout;
-  let qIsDirectQueryMode;
-  let qUnsupportedFeature;
+  let qUnsupportedFeatures;
 
   beforeEach(() => {
     sandbox = sinon.createSandbox();
-    qIsDirectQueryMode = false;
-    qUnsupportedFeature = [];
+    qUnsupportedFeatures = [];
     layout = { snapshotData: 'some-data', qHyperCube: 'hpc' };
     sandbox.stub(chartModule, 'getValue');
     sandbox
@@ -23,36 +21,16 @@ describe('meta', () => {
     flags = { isEnabled: sandbox.stub() };
     flags.isEnabled.withArgs('NUM_BUBBLES').returns(false);
     flags.isEnabled.withArgs('PROGRESSIVE_RENDERING').returns(false);
-    create = () => createMeta(flags, qIsDirectQueryMode, qUnsupportedFeature);
+    create = () => createMeta(flags, qUnsupportedFeatures);
   });
 
   afterEach(() => {
     sandbox.restore();
   });
 
-  it('should return correct meta object, when it is not BDI live mode', () => {
+  it('should return correct meta object', () => {
     chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
     chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(101);
-    expect(create()({ layout })).to.deep.equal({
-      isSnapshot: true,
-      hasSizeMeasure: true,
-      isBigData: true,
-      isContinuous: true,
-      isRangeSelectionsSupported: true,
-      isMaxVisibleBubblesEnabled: false,
-      isProgressiveEnabled: false,
-      isLargeNumDataPoints: false,
-      maxVisibleBubbles: 100,
-      largeNumDataPoints: 100,
-      numDataPoints: 101,
-    });
-  });
-
-  it('should return correct meta object, when it is BDI live mode but BDI flag is not enabled', () => {
-    chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
-    chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(101);
-    layout.qIsBDILiveMode = true;
-    flags.isEnabled.withArgs('BDI_CLIENT_ADAPT').returns(false);
     expect(create()({ layout })).to.deep.equal({
       isSnapshot: true,
       hasSizeMeasure: true,
@@ -69,41 +47,41 @@ describe('meta', () => {
   });
 
   it('should return correct meta object, when binning is not supported', () => {
-    qIsDirectQueryMode = true;
-    qUnsupportedFeature = ['binningData'];
+    qUnsupportedFeatures = ['binningData'];
     chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
-    chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(101);
+    chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(15000);
     expect(create()({ layout })).to.deep.equal({
       isSnapshot: true,
       hasSizeMeasure: true,
       isBigData: false,
       isContinuous: true,
-      isRangeSelectionsSupported: false,
+      isRangeSelectionsSupported: true,
       isMaxVisibleBubblesEnabled: false,
       isProgressiveEnabled: false,
       isLargeNumDataPoints: false,
       maxVisibleBubbles: 100,
       largeNumDataPoints: 100,
-      numDataPoints: 101,
+      numDataPoints: 15000,
     });
   });
 
   it('should return correct meta object, when range selections is not supported', () => {
-    qIsDirectQueryMode = true;
-    qUnsupportedFeature = ['rangeSelections'];
+    qUnsupportedFeatures = ['rangeSelections'];
     chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
     chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(101);
+    flags.isEnabled.withArgs('NUM_BUBBLES').returns(true);
+    layout.maxVisibleBubbles = 300;
     expect(create()({ layout })).to.deep.equal({
       isSnapshot: true,
       hasSizeMeasure: true,
       isBigData: false,
       isContinuous: true,
       isRangeSelectionsSupported: false,
-      isMaxVisibleBubblesEnabled: false,
+      isMaxVisibleBubblesEnabled: true,
       isProgressiveEnabled: false,
       isLargeNumDataPoints: false,
-      maxVisibleBubbles: 100,
-      largeNumDataPoints: 100,
+      maxVisibleBubbles: 300,
+      largeNumDataPoints: 300,
       numDataPoints: 101,
     });
   });
@@ -111,8 +89,6 @@ describe('meta', () => {
   it('should return correct meta object, when NUM_BUBBLES flag is enabled and qcy < maxVisibleBubbles < LARGE_NUM_DATA_POINTS', () => {
     chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
     chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(101);
-    layout.qIsBDILiveMode = true;
-    flags.isEnabled.withArgs('BDI_CLIENT_ADAPT').returns(false);
     flags.isEnabled.withArgs('NUM_BUBBLES').returns(true);
     layout.maxVisibleBubbles = 300;
     expect(create()({ layout })).to.deep.equal({
@@ -133,8 +109,6 @@ describe('meta', () => {
   it('should return correct meta object, when NUM_BUBBLES flag is enabled and maxVisibleBubbles < qcy < LARGE_NUM_DATA_POINTS', () => {
     chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
     chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(400);
-    layout.qIsBDILiveMode = true;
-    flags.isEnabled.withArgs('BDI_CLIENT_ADAPT').returns(false);
     flags.isEnabled.withArgs('NUM_BUBBLES').returns(true);
     layout.maxVisibleBubbles = 300;
     expect(create()({ layout })).to.deep.equal({
@@ -155,8 +129,6 @@ describe('meta', () => {
   it('should return correct meta object, when NUM_BUBBLES flag is enabled and maxVisibleBubbles < LARGE_NUM_DATA_POINTS < qcy', () => {
     chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
     chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(600);
-    layout.qIsBDILiveMode = true;
-    flags.isEnabled.withArgs('BDI_CLIENT_ADAPT').returns(false);
     flags.isEnabled.withArgs('NUM_BUBBLES').returns(true);
     layout.maxVisibleBubbles = 300;
     expect(create()({ layout })).to.deep.equal({
@@ -177,8 +149,6 @@ describe('meta', () => {
   it('should return correct meta object, when NUM_BUBBLES flag is enabled and qcy < LARGE_NUM_DATA_POINTS < maxVisibleBubbles', () => {
     chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
     chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(101);
-    layout.qIsBDILiveMode = true;
-    flags.isEnabled.withArgs('BDI_CLIENT_ADAPT').returns(false);
     flags.isEnabled.withArgs('NUM_BUBBLES').returns(true);
     layout.maxVisibleBubbles = 600;
     expect(create()({ layout })).to.deep.equal({
@@ -199,8 +169,6 @@ describe('meta', () => {
   it('should return correct meta object, when NUM_BUBBLES flag is enabled and LARGE_NUM_DATA_POINTS < qcy < maxVisibleBubbles', () => {
     chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
     chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(550);
-    layout.qIsBDILiveMode = true;
-    flags.isEnabled.withArgs('BDI_CLIENT_ADAPT').returns(false);
     flags.isEnabled.withArgs('NUM_BUBBLES').returns(true);
     layout.maxVisibleBubbles = 600;
     expect(create()({ layout })).to.deep.equal({
@@ -221,8 +189,6 @@ describe('meta', () => {
   it('should return correct meta object, when NUM_BUBBLES flag is enabled and LARGE_NUM_DATA_POINTS < maxVisibleBubbles < qcy', () => {
     chartModule.getValue.withArgs('hpc', 'qMeasureInfo.2').returns('some-info');
     chartModule.getValue.withArgs('hpc', 'qSize.qcy').returns(800);
-    layout.qIsBDILiveMode = true;
-    flags.isEnabled.withArgs('BDI_CLIENT_ADAPT').returns(false);
     flags.isEnabled.withArgs('NUM_BUBBLES').returns(true);
     layout.maxVisibleBubbles = 600;
     expect(create()({ layout })).to.deep.equal({
