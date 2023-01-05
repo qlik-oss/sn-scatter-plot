56691cd02b21e6980bcd6b43bad45d52f14799e3
Per Nylander <per.nylander@qlik.com>
2023-01-05 10:47:05 +0100

    test: brushing tests added

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -2,7 +2,7 @@ import path from 'path';
 import serve from '@nebula.js/cli-serve';
 import { test, expect, chromium } from '@playwright/test';
 import createNebulaRoutes from '../utils/routes';
-import getTooltipContent from '../utils/shared';
+import { getTooltipContent, checkScreenshotBrushing, drag } from '../utils/shared';
 
 test.describe('sn scatter plot: ui integration tests to test visual bugs', () => {
   let s;
@@ -55,6 +55,108 @@ test.describe('sn scatter plot: ui integration tests to test visual bugs', () =>
         page.locator('[data-key="heat-map"] g rect[data-value="436904"]');
         await page.hover('[data-key="heat-map"] g rect[data-value="436904"]');
         expect(await getTooltipContent(page)).toEqual('Density: 3805');
+      });
+    });
+    test.describe.only('Brush', () => {
+      test('Select by point ', async () => {
+        const renderUrl = await route.renderFixture('scatter_render_tooltip.fix.js');
+        // Open page in Nebula which renders fixture
+        // render svg added to the fix.js
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(renderUrl, { waitUntil: 'networkidle' });
+        await page.click('[data-key="point-component"] g circle[data-label="Karl Anderson"]');
+        await checkScreenshotBrushing('[data-key="point-component"]', page, 'brush_point_selection.png');
+      });
+      test('Select by categorical legend ', async () => {
+        const renderUrl = await route.renderFixture('scatter_render_tooltip.fix.js');
+        // Open page in Nebula which renders fixture
+        // render svg added to the fix.js
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(renderUrl, { waitUntil: 'networkidle' });
+
+        const source = await page.waitForSelector('[data-key="legend-color-cat"] g g[data-label="Craig Lary"]');
+        const sourceRect = await source.boundingBox();
+        const clickArea = {
+          x: Math.ceil(sourceRect.x + sourceRect.width / 2),
+          y: Math.ceil(sourceRect.y + sourceRect.height / 2),
+        };
+        await page.mouse.click(clickArea.x, clickArea.y);
+        await checkScreenshotBrushing('[data-key="point-component"]', page, 'brush_legend_selection.png');
+      });
+      test('Select by Y-axis range ', async () => {
+        const renderUrl = await route.renderFixture('scatter_render_tooltip.fix.js');
+        // Open page in Nebula which renders fixture
+        // render svg added to the fix.js
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(renderUrl, { waitUntil: 'networkidle' });
+
+        await page.waitForSelector('svg[data-key="y-axis"]', { visible: true });
+        const yAxisLabels = await page.$$('svg[data-key="y-axis"] text');
+        const labelsMap = await page.$$eval('svg[data-key="y-axis"] text', (ticks) =>
+          ticks.map((tick) => tick.textContent === '2' || tick.textContent === '6')
+        );
+        const range = yAxisLabels.filter((label, j) => labelsMap[j]);
+        const lowerRect = await range[0].boundingBox();
+        const upperRect = await range[1].boundingBox();
+
+        const from = {
+          x: lowerRect.x,
+          y: lowerRect.y + lowerRect.height / 2,
+        };
+
+        const to = {
+          x: upperRect.x,
+          y: upperRect.y + upperRect.height / 2,
+        };
+        await drag(page, from, to, { steps: 20 });
+        await checkScreenshotBrushing('[data-key="point-component"]', page, 'brush_y_axis_range_selection.png');
+      });
+      test('Select by X-axis range ', async () => {
+        const renderUrl = await route.renderFixture('scatter_render_tooltip.fix.js');
+        // Open page in Nebula which renders fixture
+        // render svg added to the fix.js
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(renderUrl, { waitUntil: 'networkidle' });
+
+        await page.waitForSelector('svg[data-key="x-axis"]', { visible: true });
+        const yAxisLabels = await page.$$('svg[data-key="x-axis"] text');
+        const labelsMap = await page.$$eval('svg[data-key="x-axis"] text', (ticks) =>
+          ticks.map((tick) => tick.textContent === '$2,000,000.00' || tick.textContent === '$8,000,000.00')
+        );
+        const range = yAxisLabels.filter((label, j) => labelsMap[j]);
+        const lowerRect = await range[0].boundingBox();
+        const upperRect = await range[1].boundingBox();
+
+        const from = {
+          x: lowerRect.x,
+          y: lowerRect.y + lowerRect.height / 2,
+        };
+
+        const to = {
+          x: upperRect.x,
+          y: upperRect.y + upperRect.height / 2,
+        };
+        await drag(page, from, to, { steps: 20 });
+        await checkScreenshotBrushing('[data-key="point-component"]', page, 'brush_x_axis_range_selection.png');
+      });
+      test('Select bin ', async () => {
+        const renderUrl = await route.renderFixture('scatter_render_binned_snapshot_tooltip.fix.js');
+        // Open page in Nebula which renders fixture
+        // render svg added to the fix.js
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(renderUrl, { waitUntil: 'networkidle' });
+        await page.click('[data-key="heat-map"] g rect[data-value="436904"]');
+        await checkScreenshotBrushing('[data-key="heat-map"]', page, 'brush_bin_selection.png');
       });
     });
     test.describe('Zoom', () => {
