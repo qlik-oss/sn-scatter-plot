21c0b0e126bb27066a5315362ca41f15477efe1e
Per Nylander <per.nylander@qlik.com>
2023-01-05 11:24:02 +0100

    test: brushing tests added (#366)
    * test: brushing tests added
    
    * test: removed only
    
    * test: adding artifacts
    
    * test: add baselines

--- test/utils/shared.js
+++ test/utils/shared.js
@@ -1,4 +1,6 @@
-export default async function getTooltipContent(page) {
+import { expect } from '@playwright/test';
+
+async function getTooltipContent(page) {
   // eslint-disable-next-line no-undef
   const tooltip = await page.waitForSelector('.pic-tooltip', { visible: true });
   const tooltipContent = await tooltip.$$eval('.pic-tooltip-content tr td', (dataEntries) =>
@@ -6,3 +8,18 @@ export default async function getTooltipContent(page) {
   );
   return tooltipContent.join(' ');
 }
+
+async function checkScreenshotBrushing(selector, page, nameOfFile) {
+  const element = page.locator(selector, { state: 'visible' });
+  const image = await page.screenshot({ clip: await element.boundingBox() });
+  expect(image).toMatchSnapshot(nameOfFile);
+}
+
+async function drag(page, from, to, options) {
+  await page.mouse.move(from.x, from.y);
+  await page.mouse.down();
+  await page.mouse.move(to.x, to.y, options);
+  await page.mouse.up();
+}
+
+export { getTooltipContent, checkScreenshotBrushing, drag };
